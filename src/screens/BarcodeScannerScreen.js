628e4fcfb9aa36cb803e1a41dbc91952d5555cc2
Nayana Limaye <nalimaye@yahoo.com>
2019-10-07 11:51:26 -0400

    feat: User can scan barcode & the name gets added to Ingredients List

--- src/screens/BarcodeScannerScreen.js
+++ src/screens/BarcodeScannerScreen.js
@@ -6,11 +6,9 @@ import buycott from '../api/buycott';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { withNavigation } from 'react-navigation';
 
-
 const BarcodeScannerScreen = ({ navigation }) => {
   const [hasCameraPermission, setCameraPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [dataFromBarcode, setDataFromBarcode] = useState('');
 
   const getPermissionsAsync = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -19,37 +17,36 @@ const BarcodeScannerScreen = ({ navigation }) => {
     }
   };
 
-  const searchBarcodeApi = async (barcode) => {
+  // Search Buycott API to retrieve the product name, for which the user has scanned the barcode.
+  const searchBarcodeApi = async barcode => {
     try {
-      const {data} = await buycott.get('/', {
+      const { data } = await buycott.get('/', {
         params: {
           barcode: barcode,
-          access_token: BUYCOTT_API
-        }
+          access_token: BUYCOTT_API,
+        },
       });
-      console.log('data', data)
-      if(data.products[0].product_name) {
-        console.log('HERE', typeof(data.products[0].product_name));
-        
-        ( () => { setDataFromBarcode('Keebler'); } )()
-        console.log('data in barcode', dataFromBarcode);
-        navigation.navigate('Search', {ingredientName: dataFromBarcode});
+      // If we have a product name,
+      if (data.products[0].product_name) {
+        // Extract the product name.
+        const productName = data.products[0].product_name;
+        // Navigate back to Search screen with the found product name.
+        navigation.navigate('Search', { ingredientName: productName });
       }
-    } catch (error) { 
-      console.log('Error!, ', error)
+    } catch (error) {
+      console.log('Error!, ', error);
     }
-  }
+  };
 
   const handleBarCodeScanned = async ({ type, data }) => {
     setScanned(true);
     searchBarcodeApi(data);
-    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);    
+    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 
   useEffect(() => {
     getPermissionsAsync();
-    // searchBarcodeApi('070129290781');
-  })
+  });
 
   if (hasCameraPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -64,7 +61,8 @@ const BarcodeScannerScreen = ({ navigation }) => {
         flex: 1,
         flexDirection: 'column',
         justifyContent: 'flex-end',
-      }}>
+      }}
+    >
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
@@ -74,7 +72,7 @@ const BarcodeScannerScreen = ({ navigation }) => {
         <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
       )}
     </View>
-  )
-}
+  );
+};
 
 export default withNavigation(BarcodeScannerScreen);
