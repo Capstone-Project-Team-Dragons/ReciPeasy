10d0c6fbc455838426ee65bbb3431498453b5331
Gundeep Kohli <gkohli1@fordham.edu>
2019-10-08 17:35:04 -0400

    feature: added tab navigation and login component - still need to integrate it to MyRecipes page

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchIngredientsScreen from './src/screens/SearchIngredientsScreen';
 import SingleRecipeScreen from './src/screens/SingleRecipeScreen';
 import BarcodeScannerScreen from './src/screens/BarcodeScannerScreen';
 import InstructionScreen from './src/screens/InstructionsScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import MyRecipesScreen from './src/screens/MyRecipesScreen';
 
-const navigator = createStackNavigator(
-  { 
-    Search: SearchIngredientsScreen,
-    SingleRecipe: SingleRecipeScreen,
-    BarcodeScanner: BarcodeScannerScreen, 
-    Instruction: InstructionScreen
-  }, 
+const SearchRecipesStack = createStackNavigator(
+  {
+  Search: SearchIngredientsScreen,
+  SingleRecipe: SingleRecipeScreen,
+  BarcodeScanner: BarcodeScannerScreen, 
+  Instruction: InstructionScreen
+  },   
   {
     initialRouteName: 'Search',
     defaultNavigationOptions: {
@@ -20,5 +23,28 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator)
+const MyRecipesStack = createStackNavigator(
+  { 
+    MyRecipes: MyRecipesScreen,
+    Login: LoginScreen
+  },   
+  {
+    initialRouteName: 'MyRecipes',
+    defaultNavigationOptions: {
+      title: "My Recipes"
+    }
+  }
+);
+
+export default createAppContainer(
+  createBottomTabNavigator(
+    {
+      'Search Recipes': SearchRecipesStack,
+      'My Recipes': MyRecipesStack,
+    }, 
+    {
+
+    }
+  )
+)
 
