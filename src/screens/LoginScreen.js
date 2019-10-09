39a28800c6e56364f54e021e92f6be99b0ba46d5
Nayana Limaye <nalimaye@yahoo.com>
2019-10-09 11:33:46 -0400

    fix: fixed firebase project related parameters for login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,44 +1,43 @@
 import React from 'react';
-import * as firebase from "firebase";
+import * as firebase from 'firebase';
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import { FIREBASE_API } from 'react-native-dotenv';
 
 var config = {
-    apiKey: `${FIREBASE_API}`,
-    authDomain: "recipeasy-a93a5.firebaseapp.com",
-    databaseURL: "https://recipeasy-a93a5.firebaseapp.com",
-    projectId: "recipeasy-a93a5",
-    storageBucket: "recipeasy-a93a5.appspot.com",
-     messagingSenderId: "155268471548"
+  apiKey: `${FIREBASE_API}`,
+  authDomain: 'ingredia-dc183.firebaseapp.com',
+  databaseURL: 'https://ingredia-dc183.firebaseapp.com',
+  projectId: 'ingredia-dc183',
+  storageBucket: 'ingredia-dc183.appspot.com',
+  messagingSenderId: '1083112709962',
 };
 
 firebase.initializeApp(config);
 
 export default class Login extends React.Component {
-  state = { email: '', password: '', errorMessage: null }
+  state = { email: '', password: '', errorMessage: null };
   handleLogin = () => {
     // TODO: Firebase stuff...
     // console.log('handleLogin')
     try {
-        firebase
-           .auth()
-           .signInWithEmailAndPassword(this.state.email, this.state.password)
-           .then(res => {
-               console.log(res.user.email);
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(res => {
+          console.log(res.user.email);
         });
-     } catch (error) {
-        console.log(error.toString(error));
+    } catch (error) {
+      console.log(error.toString(error));
     }
-  }
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <Text>Login</Text>
-        {this.state.errorMessage &&
-          <Text style={{ color: 'red' }}>
-            {this.state.errorMessage}
-          </Text>}
+        {this.state.errorMessage && (
+          <Text style={{ color: 'red' }}>{this.state.errorMessage}</Text>
+        )}
         <TextInput
           style={styles.textInput}
           autoCapitalize="none"
@@ -67,13 +66,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   textInput: {
     height: 40,
     width: '90%',
     borderColor: 'gray',
     borderWidth: 1,
-    marginTop: 8
-  }
-})
+    marginTop: 8,
+  },
+});
