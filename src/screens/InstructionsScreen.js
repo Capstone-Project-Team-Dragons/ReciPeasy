ddfd4c3e348a2f1d0e3ecbb6e677505cdeeda9a2
KateL <kelee9363@yahoo.com>
2019-10-09 15:50:19 -0400

    feat: add additional info to the recipe detail screen

--- src/screens/InstructionsScreen.js
+++ src/screens/InstructionsScreen.js
@@ -4,9 +4,15 @@ import InstructionList from '../components/InstructionList';
 
 const InstructionScreen = ({ navigation}) => {
     const instructions = navigation.getParam('instructions');
+    const addtionalInfo = navigation.getParam('addtionalInfo');
     return (
         <View style={styles.container}>
             <Text  style={styles.textTitle}>Instruction Details</Text>
+            <View style={styles.addtional}>
+                 <Text style={styles.textAddtional}>Ready In Minutes: { addtionalInfo["readyInMinutes"]}</Text>
+                 <Text style={styles.textAddtional}>Health Score: {addtionalInfo["healthScore"]}</Text>
+                 <Text style={styles.textAddtional}>Servings: {addtionalInfo["servings"]}</Text>
+            </View>
             <FlatList
                     data={instructions}
                     keyExtractor={(item, index) => 'key2'+index}
@@ -25,6 +31,19 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: "bold",
         margin: 5
+    },
+    addtional: {
+        marginLeft: 20,
+        marginRight: 20,
+        fontWeight: "bold",
+        borderStyle: "solid",
+        borderRadius: 5,
+        borderWidth: 1,
+        padding: 5
+    },
+    textAddtional: {
+        fontSize: 12,
+        fontWeight: "bold"
     }
 });
 
